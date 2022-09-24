991ff999491d3aee5f2894735daf46944f672b04
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-24 12:10:49 -0400

    Added Basic Func w/ Adding Todo + Working on Edit.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  
+  const [todo, setTodo] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(updatedTodos);
+  };
+
+  const toggleComplete = (id) => {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const editTodo = (id) => {
+
+  };
 
   return (
     <div className="App">
-      Hello World
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todoEditing === todo.id ? (
+            <input
+              type="text"
+              onChange={(e) => setEditingText(e.target.value)}
+              value={editingText}
+            />
+          ) : (
+            <div>{todo.text}</div>
+          )}
+
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          <input
+            type="checkbox"
+            onChange={() => toggleComplete(todo.id)}
+            checked={todo.completed}
+          />
+          <button onClick={() => setTodoEditing(todo.id)}>Edit Todo</button>
+          <button onClick={() => editTodo(todo.id)}>Submit Todo</button>
+        </div>
+      ))}
     </div>
   );
 }
